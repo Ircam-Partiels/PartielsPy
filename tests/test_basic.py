03ee871f75fd79688d14ea40368df7e89ce20001
totolabrico <thomas-barbe@hotmail.com>
2025-05-09 14:02:05 +0200

    Add test_basic.py and tests folders

---
+++ tests/test_basic.py
@@ -0,0 +1,61 @@
+import pytest
+from pathlib import Path
+import pkg_resources
+import os
+
+from PartielsPy.Partiels import Partiels
+
+def test_create_instance():
+    partiels = Partiels()
+    print(partiels.getExecPath())
+    assert partiels.getExecPath() is not None, "Partiels Executable not find"
+
+def test_check_version():
+    partiels = Partiels()
+    assert partiels.isHandledVersion == True, "Partiels Executable Version does not match the wrapper" 
+
+
+def exports(partiels, document, dest):
+    
+    jpeg_exporter = partiels.createJpegExporter()
+    assert jpeg_exporter.export(document, dest) == 0, "Export JPEG FAILED"
+    
+    png_exporter = partiels.createPngExporter()
+    assert png_exporter.export(document, dest) == 0, "Export PNG FAILED"
+    
+    json_exporter = partiels.createJsonExporter()
+    assert json_exporter.export(document, dest) == 0, "Export JSON FAILED"
+    
+    csv_exporter = partiels.createCsvExporter()
+    assert csv_exporter.export(document, dest) == 0, "Export CSV FAILED"
+
+    lab_exporter = partiels.createLabExporter()
+    assert lab_exporter.export(document, dest) == 0, "Export LAB FAILED"
+    
+    '''
+    cue_exporter = partiels.createCueExporter()
+    err = cue_exporter.export(document, dest)
+    assert err == 0, "Export CUE FAILED, ERROR CODE" + str(err)
+    
+    reaper_exporter = partiels.createReaperExporter()
+    assert reaper_exporter.export(document, dest) == 0, "Export REAPER FAILED"
+    '''
+
+    sdif_exporter = partiels.createSdifExporter()
+    err = sdif_exporter.export(document, dest)
+    assert  err == 0, "Export SDIF FAILED. ERROR CODE:" + str(err)
+    
+
+def test_export():
+    partiels = Partiels()
+    root = pkg_resources.resource_filename(__name__, './')
+    audiofile = root + 'samples/Sound.wav'
+    dest = root + 'exports/'
+    document = partiels.createDefaultDocument(audiofile, 'factory')
+    exports(partiels, document, dest)
+    document = partiels.createDefaultDocument(audiofile, 'supervp')
+    exports(partiels, document, dest)
+    document = partiels.createDefaultDocument(audiofile, 'partials')
+    exports(partiels, document, dest)
+
+#test_export()
