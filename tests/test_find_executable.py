2addbfb94400c95de65439320c126f7dffdc9da2
totolabrico <thomas-barbe@hotmail.com>
2025-05-13 15:15:07 +0200

    Add tests/test_find_executable.py

---
+++ tests/test_find_executable.py
@@ -0,0 +1,6 @@
+from PartielsPy.Partiels import Partiels
+
+
+def test_find_executable():
+    assert Partiels().getExecutablePath() is not None, "Partiels Executable not found"
+    print(Partiels().getExecutablePath() + " v" + Partiels().getExecutableVersion())
