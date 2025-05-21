d0d58ac39a1cdf10522f248a682da2d11cd57299
totolabrico <thomas-barbe@hotmail.com>
2025-05-21 11:48:12 +0200

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PartielsPy"
+copyright = "2025, Pierre Guillot - Thomas Barbé"
+author = "Pierre Guillot - Thomas Barbé"
+release = "0.0.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+
+exclude_patterns = ["_build", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
