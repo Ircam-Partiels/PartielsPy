31849870aeb4eb37a9f3442cd68edd36d5d750df
totolabrico <thomas-barbe@hotmail.com>
2025-05-21 18:27:25 +0200

    Add src/PartielsPy/export_configs/base.py

---
+++ src/PartielsPy/export_configs/base.py
@@ -0,0 +1,33 @@
+"""An abstract class for export configuration"""
+
+from abc import ABC, abstractmethod
+
+
+class ExportConfigBase(ABC):
+    """Export base configuration class
+
+    This is the base class used to configure the export of the document. Is not intended to \
+    be used directly but inherited by the ExportConfig classes:
+    - :class:`PartielsPy.export_configs.image`
+
+    Args:
+        adapt_to_sample_rate (bool): the block size and the step size of the analyzes \
+        are adapted following the sample rate (default: False)
+    """
+
+    def __init__(self, adapt_to_sample_rate: bool = False):
+        self.__adapt_to_sample_rate = adapt_to_sample_rate
+
+    @property
+    def adapt_to_sample_rate(self) -> bool:
+        return self.__adapt_to_sample_rate
+
+    @adapt_to_sample_rate.setter
+    def adapt_to_sample_rate(self, value: bool):
+        self.__adapt_to_sample_rate = value
+
+    @abstractmethod
+    def to_cli_args(self) -> list[str]:
+        if self.adapt_to_sample_rate:
+            return ["--adapt"]
+        return []
