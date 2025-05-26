9c6cbb87fef1b9dbd04a64221922452e9b3e0baa
totolabrico <thomas-barbe@hotmail.com>
2025-05-26 17:46:52 +0200

    Add src/PartielsPy/export_configs/reaper.py

---
+++ src/PartielsPy/export_configs/reaper.py
@@ -0,0 +1,43 @@
+"""A class for Reaper export configuration"""
+
+from enum import StrEnum, auto
+
+from .base import ExportConfigBase
+
+
+class ExportConfigReaper(ExportConfigBase):
+    """Reaper export configuration class
+
+    This class is used to configure the export of files in Reaper format.
+    it is used in the export method of the :class:`PartielsPy.partiels` class.
+
+    Args:
+        reaper_type (ExportConfigReaper.ReaperTypes): the type of the reaper format\
+        (default: REGION)
+    """
+
+    class ReaperTypes(StrEnum):
+        REGION = auto()
+        MARKER = auto()
+
+    def __init__(
+        self,
+        reaper_type: ReaperTypes = ReaperTypes.REGION,
+        adapt_to_sample_rate: bool = False,
+    ):
+        super().__init__(adapt_to_sample_rate=adapt_to_sample_rate)
+        self.reaper_type = reaper_type
+
+    @property
+    def reaper_type(self) -> ReaperTypes:
+        return self.__reaper_type
+
+    @reaper_type.setter
+    def reaper_type(self, value: ReaperTypes):
+        self.__reaper_type = ExportConfigReaper.ReaperTypes(value)
+
+    def to_cli_args(self) -> list[str]:
+        res = super().to_cli_args()
+        res.append("--format=reaper")
+        res.append("--reapertype=" + self.reaper_type.value)
+        return res
