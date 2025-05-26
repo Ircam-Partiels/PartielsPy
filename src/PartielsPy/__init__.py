0bcd1e00740ec1a11cd27752aab38afa34fffdd0
totolabrico <thomas-barbe@hotmail.com>
2025-05-26 16:14:38 +0200

    src/PartielsPy/__init__.py : expose new ExportConfig classes

--- src/PartielsPy/__init__.py
+++ src/PartielsPy/__init__.py
@@ -1,4 +1,17 @@
+from .export_configs.csv import ExportConfigCsv
+from .export_configs.cue import ExportConfigCue
 from .export_configs.image import ExportConfigImage
+from .export_configs.json import ExportConfigJson
+from .export_configs.lab import ExportConfigLab
+from .export_configs.reaper import ExportConfigReaper
 from .partiels import Partiels
 
-__all__ = ["Partiels", "ExportConfigImage"]
+__all__ = [
+    "Partiels",
+    "ExportConfigImage",
+    "ExportConfigCsv",
+    "ExportConfigReaper",
+    "ExportConfigJson",
+    "ExportConfigLab",
+    "ExportConfigCue",
+]
