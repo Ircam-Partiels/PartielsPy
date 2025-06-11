590ed8b63c4d566db3cd9f2051971240758d72c2
totolabrico <thomas-barbe@hotmail.com>
2025-06-11 14:38:48 +0200

    examples: add examples for plugin_list and document

---
+++ examples/document_track_with_file.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from partielspy import *
+
+folder = Path(__file__).parent
+resource = folder.parent / "resource"
+
+partiels = Partiels()
+
+document = Document()
+group = Group("Group")
+track = Track("Track")
+track.file_info = FileInfo(resource / "marker.csv")
+group.add_track(track)
+document.add_group(group)
+
+audio_file = resource / "Sound.wav"
+config = ExportConfigJson()
+output = folder / "exports" / "document_track_with_file"
+partiels.export(audio_file, document, output, config)
