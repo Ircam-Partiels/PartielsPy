bb9da0e90e7bfc9889dd264dbcc23db8d0e6272c
totolabrico <thomas-barbe@hotmail.com>
2025-06-11 14:36:48 +0200

    Add src/partielspy/document.py

---
+++ src/partielspy/document.py
@@ -0,0 +1,58 @@
+import uuid
+
+from lxml import etree
+
+from .group import Group
+
+
+class Document:
+    def __init__(self):
+        self.__MiscModelVersion = None
+        self.__groups = {}
+
+    @property
+    def groups(self) -> list[Group]:
+        return list(self.__groups.values())
+
+    def add_group(self, group: Group):
+        if not isinstance(group, Group):
+            raise TypeError("Expected a Group instance")
+        self.__groups[uuid.uuid4().hex] = group
+
+    def remove_group(self, group: Group):
+        for key, value in self.__groups.items():
+            if value == group:
+                del self.__groups[key]
+                return
+        raise ValueError("Group not found in document")
+
+    def _to_xml(self, root: etree, MiscModelVersion: str):
+        MiscModelVersion = (
+            MiscModelVersion if not self.__MiscModelVersion else self.__MiscModelVersion
+        )
+        root.set("MiscModelVersion", MiscModelVersion)
+
+        def element_to_xml(
+            parent_node: etree, identifier: str, element: object, tag: str
+        ):
+            layout = etree.Element("layout")
+            layout.set("value", identifier)
+            parent_node.append(layout)
+            element_node = etree.Element(tag)
+            element_node.set("identifier", identifier)
+            element_node.set("MiscModelVersion", MiscModelVersion)
+            element._to_xml(element_node)
+            root.append(element_node)
+            return element_node
+
+        for group_identifier, group in self.__groups.items():
+            group_node = element_to_xml(root, group_identifier, group, "groups")
+            for track_identifier, track in group._tracks_items.items():
+                element_to_xml(group_node, track_identifier, track, "tracks")
+
+    def __str__(self):
+        res = ""
+        for group in self.groups:
+            res += group.__str__()
+            res += "\n" if group != self.groups[-1] else ""
+        return res
