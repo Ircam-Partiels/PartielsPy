e8613c7af6992799eaf31414374a076b163dca52
totolabrico <thomas-barbe@hotmail.com>
2025-06-11 14:38:16 +0200

    src/partielspy/__init__.py: expose new Classes

--- src/partielspy/__init__.py
+++ src/partielspy/__init__.py
@@ -1,10 +1,13 @@
+from .document import Document
 from .export_configs.csv import ExportConfigCsv
 from .export_configs.cue import ExportConfigCue
 from .export_configs.image import ExportConfigImage
 from .export_configs.json import ExportConfigJson
 from .export_configs.lab import ExportConfigLab
 from .export_configs.reaper import ExportConfigReaper
+from .group import Group
 from .partiels import Partiels
+from .track import Track
 
 __all__ = [
     "Partiels",
@@ -14,4 +17,7 @@ __all__ = [
     "ExportConfigJson",
     "ExportConfigLab",
     "ExportConfigCue",
+    "Document",
+    "Group",
+    "Track",
 ]
