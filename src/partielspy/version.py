9edccb7400c28c87352439dcfe87cff516fa7ddd
totolabrico <thomas-barbe@hotmail.com>
2025-06-12 17:07:47 +0200

    Add src/partielspy/version.py

---
+++ src/partielspy/version.py
@@ -0,0 +1,14 @@
+import semver
+
+
+class Version:
+    __compatibility_version = "2.0.11"
+
+    @staticmethod
+    def get_compatibility_version():
+        return Version.__compatibility_version
+
+    @staticmethod
+    def get_compatibility_version_int():
+        version = semver.VersionInfo.parse(Version.__compatibility_version)
+        return str((version.major << 16) | (version.minor << 8) | (version.patch))
