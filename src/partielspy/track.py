8d263346eb67be009a4e08b0c3ce7b7c68f0892a
totolabrico <thomas-barbe@hotmail.com>
2025-06-12 12:27:08 +0200

    track.py: add PluginKey attribute

--- src/partielspy/track.py
+++ src/partielspy/track.py
@@ -1,9 +1,12 @@
 from lxml import etree
 
+from .plugin_key import PluginKey
+
 
 class Track:
-    def __init__(self, name: str = "New Track"):
+    def __init__(self, name: str = "New Track", key: PluginKey = PluginKey()):
         self.__name = name
+        self.__key = key
 
     @property
     def name(self) -> str:
@@ -13,8 +16,20 @@ class Track:
     def name(self, value: str):
         self.__name = value
 
+    @property
+    def key(self) -> PluginKey:
+        return self.__key
+
+    @key.setter
+    def key(self, value: PluginKey):
+        if not isinstance(value, PluginKey):
+            raise TypeError("key must be an instance of PluginKey")
+        self.__key = value
+
     def _to_xml(self, node: etree):
         node.set("name", self.__name)
+        key_node = etree.SubElement(node, "key")
+        self.__key._to_xml(key_node)
 
     def __str__(self):
-        return self.__name
+        return self.__name + " -> Plugin: " + str(self.__key)
