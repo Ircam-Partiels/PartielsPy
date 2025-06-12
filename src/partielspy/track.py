586d54ea3229adc51ccb187e2c9c8ec01d2f811c
totolabrico <thomas-barbe@hotmail.com>
2025-06-12 12:27:08 +0200

    track.py: add Plugin attribute and to_json method

--- src/partielspy/track.py
+++ src/partielspy/track.py
@@ -1,9 +1,12 @@
 from lxml import etree
 
+from .plugin import Plugin
+
 
 class Track:
     def __init__(self, name: str = "New Track"):
         self.__name = name
+        self.__plugin = Plugin()
 
     @property
     def name(self) -> str:
@@ -13,8 +16,23 @@ class Track:
     def name(self, value: str):
         self.__name = value
 
+    @property
+    def plugin(self) -> Plugin:
+        return self.__plugin
+
+    @plugin.setter
+    def plugin(self, value: Plugin):
+        if not isinstance(value, Plugin):
+            raise TypeError("plugin must be an instance of Plugin")
+        self.__plugin = value
+
     def _to_xml(self, node: etree):
         node.set("name", self.__name)
+        self.__plugin._to_xml(node)
 
-    def __str__(self):
-        return self.__name
+    def _to_json(self) -> dict:
+        res = {
+            "name": self.__name,
+            "plugin": self.__plugin._to_json(),
+        }
+        return res
