2cfeaea9b370ffc74fb9226d1c63f3f67503f995
totolabrico <thomas-barbe@hotmail.com>
2025-06-18 15:04:24 +0200

    document.py: add _from_xml method

--- src/partielspy/document.py
+++ src/partielspy/document.py
@@ -5,11 +5,15 @@ from lxml import etree
 
 from .group import Group
 from .version import Version
+from .xml_element import XmlElement
 
 
-class Document:
+class Document(XmlElement):
+    __ignored_children = ["layout", "groups", "tracks"]
+
     def __init__(self):
         self.__groups = {}
+        super().__init__(ignored_children=Document.__ignored_children)
 
     @property
     def groups(self) -> list[Group]:
@@ -29,7 +33,24 @@ class Document:
                 return
         raise ValueError("Group not found in document")
 
+    def _from_xml(self, root: etree):
+        self.from_xml(root)
+        for group_layout in root.findall("layout"):
+            layout_value = group_layout.get("value")
+            group_node = root.find(f"./groups[@identifier='{layout_value}']")
+            self.__groups[layout_value] = Group._from_xml(group_node)
+
+    @classmethod
+    def load(cls, file: bytes) -> "Document":
+        root = etree.fromstring(file)
+        if root.tag != "document":
+            raise ValueError("Invalid XML document: root tag must be 'document'")
+        document = cls()
+        document._from_xml(root)
+        return document
+
     def to_xml(self, root: etree):
+        super().to_xml(root)
         root.set("MiscModelVersion", Version.get_compatibility_version_int())
         for group_identifier, group in self.__groups.items():
             layout = etree.Element("layout")
