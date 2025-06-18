e23e26064ab55326b09fe6047ba07aea12207331
totolabrico <thomas-barbe@hotmail.com>
2025-06-18 15:04:24 +0200

    document.py: add _from_xml, _to_xml, load and save methods

--- src/partielspy/document.py
+++ src/partielspy/document.py
@@ -1,4 +1,7 @@
+import copy
 import uuid
+from pathlib import Path
+from typing import Any
 
 from lxml import etree
 
@@ -8,6 +11,7 @@ from .version import Version
 
 class Document:
     def __init__(self):
+        self.__xml_node = etree.Element("document")
         self.__groups = {}
 
     @property
@@ -28,21 +32,39 @@ class Document:
                 return
         raise ValueError("Group not found in document")
 
-    def _to_xml(self, root: etree):
+    def _from_xml(self, root: etree):
+        group_layouts = root.findall("layout")
+        for group_layout in group_layouts:
+            layout_value = group_layout.get("value")
+            group_node = root.find(f"./groups[@identifier='{layout_value}']")
+            group = Group()
+            group._from_xml(group_node)
+            self.__groups[layout_value] = group
+            root.remove(group_layout)
+            root.remove(group_node)
+        self.__xml_node = copy.deepcopy(root)
+
+    @classmethod
+    def load(cls, file: Any):
+        tree = etree.parse(file)
+        root = tree.getroot()
+        if root.tag != "document":
+            raise ValueError("Invalid XML document: root tag must be 'document'")
+        document = cls()
+        document._from_xml(root)
+        return document
+
+    def _to_xml(self) -> etree.Element:
+        root = copy.deepcopy(self.__xml_node)
         root.set("MiscModelVersion", Version.get_compatibility_version_int())
         for group_identifier, group in self.__groups.items():
             layout = etree.Element("layout")
-            root.append(layout)
             layout.set("value", group_identifier)
-            group_node = etree.Element("groups")
-            root.append(group_node)
-            group_node.set("identifier", group_identifier)
-            group_node.set("MiscModelVersion", Version.get_compatibility_version_int())
-            group._to_xml(group_node)
+            root.append(layout)
+            root.extend(group._to_xml(group_identifier))
+        return root
 
-    def __str__(self):
-        res = "Document:\n"
-        for group in self.groups:
-            res += group.__str__()
-            res += "\n" if group != self.groups[-1] else ""
-        return res
+    def save(self, file: str | Path):
+        root = self._to_xml()
+        xml = etree.ElementTree(root)
+        xml.write(file, pretty_print=True, xml_declaration=True, encoding="UTF-8")
