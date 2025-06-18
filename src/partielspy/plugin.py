754ba638af9a4cd4441e58d5f309b027eec7f805
totolabrico <thomas-barbe@hotmail.com>
2025-06-18 17:11:44 +0200

    Add src/partielspy/plugin.py

---
+++ src/partielspy/plugin.py
@@ -0,0 +1,24 @@
+from lxml import etree
+
+from .plugin_key import PluginKey
+
+
+class Plugin:
+    def __init__(self, key: PluginKey = None):
+        if key is None:
+            self.__key = PluginKey()
+        elif not isinstance(key, PluginKey):
+            raise TypeError("key must be an instance of PluginKey")
+        else:
+            self.__key = key
+
+    @property
+    def key(self) -> PluginKey:
+        return self.__key
+
+    def _to_xml(self, node):
+        key_node = etree.SubElement(node, "key")
+        self.__key._to_xml(key_node)
+
+    def _to_json(self) -> dict:
+        return {"key": self.__key._to_json()}
