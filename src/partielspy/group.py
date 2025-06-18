f3b1c607cd56eeeaea9fbe440c09c20e92bb387b
totolabrico <thomas-barbe@hotmail.com>
2025-06-18 15:04:50 +0200

    group.py: add _from_xml and _to_xml methods. Remove _to_json method

--- src/partielspy/group.py
+++ src/partielspy/group.py
@@ -9,6 +9,7 @@ from .version import Version
 
 class Group:
     def __init__(self, name: str = "New Group"):
+        self.__xml_node = etree.Element("groups")
         self.__name = name
         self.__tracks = {}
 
@@ -45,21 +46,31 @@ class Group:
         }
         return res
 
-    def _to_xml(self, node: etree):
-        node.set("name", self.__name)
-        for identifier, track in self.__tracks.items():
-            layout = etree.Element("layout")
-            node.append(layout)
-            layout.set("value", identifier)
-            track_node = etree.Element("tracks")
-            node.getparent().append(track_node)
-            track_node.set("identifier", identifier)
-            track_node.set("MiscModelVersion", Version.get_compatibility_version_int())
-            track._to_xml(track_node)
+    def _from_xml(self, node: etree.Element):
+        self.name = node.get("name", self.name)
+        track_layouts = node.findall("layout")
+        for track_layout in track_layouts:
+            track_layout_value = track_layout.get("value")
+            track_node = node.getparent().find(
+                f"./tracks[@identifier='{track_layout_value}']"
+            )
+            track = Track()
+            track._from_xml(track_node)
+            self.__tracks[track_layout_value] = track
+            node.remove(track_layout)
+            node.getparent().remove(track_node)
+        self.__xml_node = copy.deepcopy(node)
 
-    def __str__(self):
-        res = "\tGroup: " + self.name + "\n"
-        for track in self.tracks:
-            res += "\t\tTrack: " + track.__str__()
-            res += "\n" if track != self.tracks[-1] else ""
-        return res
+    def _to_xml(self, identifier: str) -> list[etree.Element]:
+        all_nodes = []
+        node = copy.deepcopy(self.__xml_node)
+        node.set("MiscModelVersion", Version.get_compatibility_version_int())
+        node.set("identifier", identifier)
+        node.set("name", self.__name)
+        all_nodes.append(node)
+        for track_identifier, track in self.__tracks.items():
+            layout = etree.Element("layout")
+            layout.set("value", track_identifier)
+            node.append(layout)
+            all_nodes.append(track._to_xml(track_identifier))
+        return all_nodes
