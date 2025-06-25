4041cbe96135ac724a2e82b33729c4ffd48448c9
totolabrico <thomas-barbe@hotmail.com>
2025-06-25 13:43:30 +0200

    plugin_list.py: add doc

--- src/partielspy/plugin_list.py
+++ src/partielspy/plugin_list.py
@@ -1,9 +1,25 @@
+"""A class to display the list of available plugins in Partiels"""
+
 from lxml import etree
 
 from .plugin_key import PluginKey
 
 
 class PluginList(list[PluginKey]):
+    """
+    This class is not intended to be instantiated directly, but rather through the
+    get_plugin_list method of the :class:`partielspy.partiels` class.
+    It contains a list of :class:`partielspy.plugin_key` objects,
+    each representing a plugin key with an identifier and a feature.
+    Its purpose is to provide a convenient way to access and display the list of plugins.
+
+    Example usage:
+        from partielspy import Partiels\n
+        partiels = Partiels()\n
+        plugin_list = partiels.get_plugin_list()\n
+        print(plugin_list)
+    """
+
     @classmethod
     def _from_xml(cls, node: etree.Element):
         return cls(
@@ -15,4 +31,5 @@ class PluginList(list[PluginKey]):
         )
 
     def __str__(self):
+        """Return a string representation of the plugin list"""
         return "\n".join([f"{item.identifier}, {item.feature}" for item in self])
