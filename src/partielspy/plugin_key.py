757b75ea43fbf9a97a7be037dc214951578ae2aa
totolabrico <thomas-barbe@hotmail.com>
2025-06-25 13:43:21 +0200

    plugin_key.py: add doc

--- src/partielspy/plugin_key.py
+++ src/partielspy/plugin_key.py
@@ -1,7 +1,22 @@
+"""A class to manage plugin keys"""
+
 from lxml import etree
 
 
 class PluginKey:
+    """
+    This class is used to represent a key for a plugin.
+    It contains an identifier and a feature, which are used to uniquely identify the key.
+    Each :class:`partielspy.track` class has a plugin key associated with it.
+    The plugin key is used to identify the plugin that is associated with the track.
+    The get_plugin_list() method of the :class:`partielspy.partiels` class returns\
+    a list of PluginKey based on all the available plugins.
+
+    Args:
+        identifier (str): the identifier of the key (default: "")
+        feature (str): the feature of the key (default: "")
+    """
+
     def __init__(self, identifier: str = "", feature: str = ""):
         self.__identifier = identifier
         self.__feature = feature
