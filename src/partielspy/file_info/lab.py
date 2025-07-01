7d98493262eebfdbb55adbf67735c74015080431
totolabrico <thomas-barbe@hotmail.com>
2025-07-01 13:01:13 +0200

    file_info/lab.py: add doc

--- src/partielspy/file_info/lab.py
+++ src/partielspy/file_info/lab.py
@@ -1,7 +1,19 @@
+"""A class for tracks LAB file information"""
+
 from .csv import FileInfoCsv
 
 
 class FileInfoLab(FileInfoCsv):
+    """This class represents LAB file information in a track.
+    It contains the path to the LAB file used by the track for the export.
+
+    Args:
+        path (str): The path to the LAB file associated with the track (default: "")
+        columns_separator (partielspy.file_info.csv.FileInfoCsv.Separators): \
+        The separator used in the LAB file (default: FileInfoCsv.Separators.TAB)
+        use_end_time (bool): Whether to use the end time in the LAB file (default: True)
+    """
+
     def __init__(
         self,
         path: str = "",
