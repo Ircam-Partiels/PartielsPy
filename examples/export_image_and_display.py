84c6b22b92dbc1ea69dec436bc15c66d87133887
totolabrico <thomas-barbe@hotmail.com>
2025-07-01 11:50:26 +0200

    fixup! examples/export_image_and_display.py

--- examples/export_image_and_display.py
+++ examples/export_image_and_display.py
@@ -9,7 +9,8 @@ audio_file = root / "resource" / "Sound.wav"
 template_file = root / "templates" / "factory.ptldoc"
 output_folder = root / "examples" / "exports" / "JPEG"
 
-Partiels().export(audio_file, template_file, output_folder, ExportConfigImage())
+document = Document.load(template_file)
+Partiels().export(audio_file, document, output_folder, ExportConfigImage())
 exported_image_spectrogram_path = output_folder / "Sound Group 1_Spectrogram.jpeg"
 image = Image.open(exported_image_spectrogram_path)
 image.show()
