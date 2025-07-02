8e80cfb3bd2fa9ae041433fec3df007442b460f3
totolabrico <thomas-barbe@hotmail.com>
2025-07-02 13:39:58 +0200

    fixup! examples/document_track_with_file_csv.py

--- examples/document_track_with_file_csv.py
+++ examples/document_track_with_file_csv.py
@@ -3,19 +3,18 @@ from pathlib import Path
 from partielspy import *
 
 folder = Path(__file__).parent
+resource = folder.parent / "resource"
 
 partiels = Partiels()
 
 document = Document()
 group = Group("Group")
 track = Track("Track")
-track.file_info = FileInfoCsv(
-    Path(folder / "results" / "document_with_file" / "src.csv")
-)
+track.file_info = FileInfoCsv(resource / "marker.csv")
 group.add_track(track)
 document.add_group(group)
 
-audio_file = folder.parent / "resource" / "Sound.wav"
+audio_file = resource / "Sound.wav"
 config = ExportConfigJson()
 output = folder / "exports" / "document_track_with_file"
 partiels.export(audio_file, document, output, config)
