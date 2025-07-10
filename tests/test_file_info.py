20698486dd07d3595711bf34645d37cdd718ce78
Pierre Guillot <guillotpierre6@gmail.com>
2025-07-10 14:14:08 +0200

    src/partielspy: add explicit csv to member names in file_info.py

--- tests/test_file_info.py
+++ tests/test_file_info.py
@@ -11,55 +11,58 @@ def test_file_info_creation():
     file_info = FileInfo()
 
     file_info = FileInfo(path=resource_dir / "marker.cue")
-    assert file_info.columns_separator is None, "The columns_separator should be None"
-    assert file_info.use_end_time is None, "The use_end_time should be None"
+    assert (
+        file_info.csv_columns_separator is None
+    ), "The csv_columns_separator should be None"
+    assert file_info.csv_use_end_time is None, "The csv_use_end_time should be None"
 
     file_info = FileInfo(path=resource_dir / "marker.csv")
     assert (
-        file_info.columns_separator == FileInfo.Separators.COMMA
-    ), "The columns_separator should be COMMA"
-    assert file_info.use_end_time is False, "The use_end_time should be False"
+        file_info.csv_columns_separator == FileInfo.CsvColumnSeparators.COMMA
+    ), "The csv_columns_separator should be COMMA"
+    assert file_info.csv_use_end_time is False, "The csv_use_end_time should be False"
 
     file_info = FileInfo(
         path=resource_dir / "marker.csv",
-        columns_separator=FileInfo.Separators.PIPE,
-        use_end_time=True,
+        csv_columns_separator=FileInfo.CsvColumnSeparators.PIPE,
+        csv_use_end_time=True,
     )
     assert (
-        file_info.columns_separator == FileInfo.Separators.PIPE
-    ), "The columns_separator should be PIPE"
-    assert file_info.use_end_time is True, "The use_end_time should be True"
+        file_info.csv_columns_separator == FileInfo.CsvColumnSeparators.PIPE
+    ), "The csv_columns_separator should be PIPE"
+    assert file_info.csv_use_end_time is True, "The csv_use_end_time should be True"
 
     file_info = FileInfo(path=resource_dir / "marker.lab")
     assert (
-        file_info.columns_separator == FileInfo.Separators.TAB
-    ), "The columns_separator should be TAB"
-    assert file_info.use_end_time is True, "The use_end_time should be True"
+        file_info.csv_columns_separator == FileInfo.CsvColumnSeparators.TAB
+    ), "The csv_columns_separator should be TAB"
+    assert file_info.csv_use_end_time is True, "The csv_use_end_time should be True"
 
     with pytest.raises(TypeError):
         file_info = FileInfo(
-            columns_separator=FileInfo.Separators.PIPE, use_end_time=True
+            csv_columns_separator=FileInfo.CsvColumnSeparators.PIPE,
+            csv_use_end_time=True,
         )
 
     with pytest.raises(ValueError):
         file_info = FileInfo(
             path=resource_dir / "marker.lab",
-            columns_separator=FileInfo.Separators.PIPE,
-            use_end_time=True,
+            csv_columns_separator=FileInfo.CsvColumnSeparators.PIPE,
+            csv_use_end_time=True,
         )
 
     with pytest.raises(TypeError):
         file_info = FileInfo(
             path=resource_dir / "marker.json",
-            columns_separator=FileInfo.Separators.PIPE,
-            use_end_time=True,
+            csv_columns_separator=FileInfo.CsvColumnSeparators.PIPE,
+            csv_use_end_time=True,
         )
 
     with pytest.raises(TypeError):
         file_info = FileInfo(
             path=resource_dir / "marker.cue",
-            columns_separator=FileInfo.Separators.PIPE,
-            use_end_time=True,
+            csv_columns_separator=FileInfo.CsvColumnSeparators.PIPE,
+            csv_use_end_time=True,
         )
 
     with pytest.raises(TypeError):
@@ -70,8 +73,8 @@ def test_file_info_creation():
 
     file_info = FileInfo(path=resource_dir / "marker.cue")
     with pytest.raises(TypeError):
-        file_info.columns_separator = FileInfo.Separators.PIPE
+        file_info.csv_columns_separator = FileInfo.CsvColumnSeparators.PIPE
 
     file_info = FileInfo(path=resource_dir / "marker.cue")
     with pytest.raises(TypeError):
-        file_info.use_end_time = False
+        file_info.csv_use_end_time = False
