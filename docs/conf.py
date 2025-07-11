a35797d0043f9ece8f8b71fe30ac476011da193e
Pierre Guillot <guillotpierre6@gmail.com>
2025-07-11 12:15:47 +0200

    Bump version to 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@ sys.path.insert(0, str(Path(__file__).resolve().parent / "../src"))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "PartielsPy"
-copyright = "2025, Pierre Guillot - Thomas Barbé"
-author = "Pierre Guillot - Thomas Barbé"
-release = "0.2.0"
+copyright = "2025 IRCAM"
+author = "Pierre Guillot and Thomas Barbé"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
